79d1c56a22953f4ceef46c7ebe932d2aec2a05a2
leerachel5 <leerachel481@gmail.com>
2024-08-09 23:00:06 -0700

    create Xcode project

---
+++ Splitwise/SplitwiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitwiseApp.swift
+//  Splitwise
+//
+//  Created by Rachel Lee on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitwiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
